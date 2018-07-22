31b9dcb9c5dc2e7eacb407d75b94e035337e1d68
Deepa Ravichandran <deepa5starbee@gmail.com>
2018-07-22 15:25:26 -0400

    Made all the changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,133 @@
+// var imageList= document.getElementsByTagName( "img" );
+// img=
+
+var curPlayers = {};
+
+
+$(document).ready(function () {
+    var availChars = $("#ROW1").children("div");
+    var idValList = createIdList(availChars);
+    var charsMap = createCharacterMap(availChars);
+
+    $("#attackButton").hide();
+
+    //When Selecting Images from Row1
+    selectYourCharacter(availChars, charsMap);
+
+    //Create Attack event
+    createAttackEvent(charsMap);
+});
+
+function selectYourCharacter(availChars, charsMap) {
+    $(availChars).each(function () {
+        $(this).on("click", function () {
+            applyCharEvent(this, availChars, charsMap);
+        })
+    });
+}
+
+function applyCharEvent(divObj, availChars, charsMap) {
+    var temp = $(divObj).children("img");
+    $("#myChar").attr("src", $(temp[0]).attr("src"));
+    curPlayers["myChar"] = $(divObj).attr("id");
+
+
+    $(divObj).remove();
+
+    $("#myCharHealth").text("Health: " + charsMap[$(divObj).attr("id")]["health"]);
+
+    $("#R1Header").text("Enemies Available to Attack");
+
+    $(availChars).each(function () {
+        $(this).off("click");
+    });
+
+    var availEnemies = $("#ROW1").children("div");
+    selectYourEnemy(availEnemies, charsMap);
+}
+
+function selectYourEnemy(availEnemies, charsMap) {
+    $(availEnemies).each(function () {
+        var temp = $(this).children("img");
+
+        $(this).on("click", function () {
+            $("#battleStatus").hide();
+            $("#defender").attr("src", $(temp[0]).attr("src"));
+
+            curPlayers["enemy"] = $(this).attr("id");
+
+            $("#defenderHealth").text("Health: " + charsMap[$(this).attr("id")]["health"]);
+            $(this).remove();
+
+            $(availEnemies).each(function () {
+                $(this).off("click");
+            });
+
+            $("#attackButton").show();
+        })
+    });
+}
+
+function createCharacterMap(availChars) {
+    var charsMap = {};
+    var healthArray = [200, 175, 125, 100];
+    var attackArray = [5, 10, 15, 20];
+    var counterArray = [10, 20, 30, 40];
+
+    var i = 0;
+    $(availChars).each(function () {
+        var idVal = $(this).attr("id");
+        var healthAndAttack = {};
+        healthAndAttack["health"] = healthArray[i];
+        healthAndAttack["initialAttack"] = attackArray[i];
+        healthAndAttack["attack"] = attackArray[i];
+        healthAndAttack["counter"] = counterArray[i];
+
+        charsMap[idVal] = healthAndAttack;
+        i++;
+    }
+    );
+    return charsMap;
+}
+
+function createIdList(availChars) {
+    var idValList = [];
+    $(availChars).each(function () {
+        idValList.push($(this).attr("id"));
+    });
+
+    return idValList;
+}
+
+function createAttackEvent(charsMap) {
+
+    $("#attackButton").on("click", function () {
+        var myCharId = curPlayers["myChar"];
+        var defenderId = curPlayers["enemy"];
+
+        charsMap[myCharId]["health"] = charsMap[myCharId]["health"] - charsMap[defenderId]["counter"];
+        charsMap[myCharId]["attack"] = charsMap[myCharId]["attack"] + charsMap[myCharId]["initialAttack"];
+        charsMap[defenderId]["health"] = charsMap[defenderId]["health"] - charsMap[myCharId]["attack"];
+
+        $("#myCharHealth").text("Health: " + charsMap[myCharId]["health"]);
+        $("#defenderHealth").text("Health: " + charsMap[defenderId]["health"]);
+
+
+        if (charsMap[defenderId]["health"] <= 0 && charsMap[myCharId]["health"] > 0) {
+            $("#attackButton").hide();
+            var availChars = $("#ROW1").children("div");
+
+            if (availChars.length > 0) {
+                $("#battleStatus").text("" + myCharId + " Defeated " + defenderId + "! Select a new Enemy");
+                selectYourEnemy(availChars, charsMap);
+            } else {
+                $("#battleStatus").text("" + myCharId + " WINS!!!! " + defenderId + "GAME OVER!");
+            }
+            $("#battleStatus").show();
+        } else if (charsMap[defenderId]["health"] > 0 && charsMap[myCharId]["health"] <= 0) {
+            $("#attackButton").hide();
+            $("#battleStatus").text("" + myCharId + "IS Defeated" + defenderId + "! GAME OVER!");
+            $("#battleStatus").show();
+        }
+    });
+}
